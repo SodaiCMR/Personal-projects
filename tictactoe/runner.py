e39027f8b38cadf857c2ed1fc5ac747f0bbd33c6
Daryl <bragouel@gmail.com>
2025-08-17 14:07:31 +0200

    tictactoe: - added and endgame screen and possibility to see if the game ended

--- tictactoe/runner.py
+++ tictactoe/runner.py
@@ -5,6 +5,7 @@ from tictactoe import *
 pygame.init()
 icon = pygame.image.load("assets/tic-tac-toe_39453.ico")
 background = pygame.image.load("assets/tictactoe_bg.png")
+end_bg = pygame.image.load("assets/tictactoe_endg.png")
 
 pygame.display.set_caption("Tic Tac Toe")
 pygame.display.set_icon(icon)
@@ -27,13 +28,16 @@ while running:
                 board[board_coords[0]][board_coords[1]] = player(board)
                 crosses.append(coords) if player(board) == X else circles.append(coords)
 
-
     # updating the screen
     screen.blit(background, (0, 0))
     for coords in crosses:
         placeCross(screen, coords)
     for coords in circles:
         placeCircle(screen, coords)
+
+    if terminal(board):
+        screen.blit(end_bg, (0, 0))
+        screen.blit(cross, (200, 80)) if winner(board) == X else screen.blit(circle, (200, 80))
 
     pygame.display.flip()
 
