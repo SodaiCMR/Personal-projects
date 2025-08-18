b3524e8406cb8d9199075f2fdf3e3208c5f3b189
Daryl <bragouel@gmail.com>
2025-08-18 10:47:55 +0200

    tictactoe: - Can't write over another case - fixed the issue with the wrong player playing

--- tictactoe/boardLogic.py
+++ tictactoe/boardLogic.py
@@ -9,15 +9,18 @@ circle = pygame.transform.scale(circle, (size[0] / 6, size[0] / 6))
 
 
 def getMouseCoord(x, y):
-    mouse_pos_x = ((x // case_width) * size[0]/3) + size[0] /12
+    mouse_pos_x = ((x // case_width) * size[0] / 3) + size[0] / 12
     mouse_pos_y = ((y // case_width) * size[0] / 3) + size[0] / 12
     return mouse_pos_x, mouse_pos_y
+
 
 def getBoardCoords(x, y):
     return int(x // case_width), int(y // case_width)
 
+
 def placeCircle(board, coord):
     board.blit(circle, coord)
+
 
 def placeCross(board, coord):
     board.blit(cross, coord)
--- tictactoe/runner.py
+++ tictactoe/runner.py
@@ -1,11 +1,11 @@
-# python
 from boardLogic import *
 from tictactoe import *
 
 pygame.init()
-icon = pygame.image.load("assets/tic-tac-toe_39453.ico")
+icon = pygame.image.load("assets/tictactoe_logo.ico")
 background = pygame.image.load("assets/tictactoe_bg.png")
-end_bg = pygame.image.load("assets/tictactoe_endg.png")
+end_bg = pygame.image.load("assets/tictactoe_win.png")
+draw_bg = pygame.image.load("assets/tictactoe_draw.png")
 
 pygame.display.set_caption("Tic Tac Toe")
 pygame.display.set_icon(icon)
@@ -13,9 +13,9 @@ screen = pygame.display.set_mode(size)
 
 crosses = []
 circles = []
+board = initial_state()
 
 running = True
-board = initial_state()
 
 while running:
     for event in pygame.event.get():
@@ -24,9 +24,12 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             coords = getMouseCoord(event.pos[0], event.pos[1])
             board_coords = getBoardCoords(event.pos[0], event.pos[1])
-            if coords is not None:
+            if coords is not None and board[board_coords[0]][board_coords[1]] is None:
                 board[board_coords[0]][board_coords[1]] = player(board)
-                crosses.append(coords) if player(board) == X else circles.append(coords)
+                if board[board_coords[0]][board_coords[1]] == X:
+                    crosses.append(coords)
+                elif board[board_coords[0]][board_coords[1]] == O:
+                    circles.append(coords)
 
     # updating the screen
     screen.blit(background, (0, 0))
@@ -35,9 +38,15 @@ while running:
     for coords in circles:
         placeCircle(screen, coords)
 
+    # endgame screen
     if terminal(board):
         screen.blit(end_bg, (0, 0))
-        screen.blit(cross, (200, 80)) if winner(board) == X else screen.blit(circle, (200, 80))
+        if winner(board) == X:
+            placeCross(screen, (200, 80))
+        elif winner(board) == O:
+            placeCircle(screen, (200, 80))
+        else:
+            screen.blit(draw_bg, (0, 0))
 
     pygame.display.flip()
 
