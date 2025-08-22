2d8dea387a96754d9519debb6ac5162541e34b9a
Daryl <bragouel@gmail.com>
2025-08-22 17:13:55 +0200

    sandbox: - resulting into coding the sun xd

--- sandbox/Ball_trail/Ball.py
+++ sandbox/Ball_trail/Ball.py
@@ -3,16 +3,17 @@ from math import cos, sin, radians
 
 class Ball:
 
-    def __init__(self):
-        self.color = (255, 255, 255)
+    def __init__(self, color = (255, 255, 255), trajectory_radius = 200):
+        self.color = color
         self.center = [480.0, 320.0]
         self.radius = 25
-        self.trajectory_radius = 200
+        self.trajectory_radius = trajectory_radius
         self.theta = 0
-        self.step = 2
+        self.step = 15
         self.frame_center = (320, 320)
         self.trail = []
         self.max_trail = 25
+        self.tours = 0
 
     def draw(self, screen):
         alpha = 255
@@ -21,6 +22,10 @@ class Ball:
             s = pygame.Surface((640, 640), pygame.SRCALPHA)
             pygame.draw.circle(s, (255, 255, 255, alpha), ball, self.radius)
             alpha -= 15 if alpha > 0 else 0
+        if self.trajectory_radius > 0 and self.theta == 0:
+            self.trajectory_radius -= 10
+        elif self.trajectory_radius == 0:
+            self.trajectory_radius = 200
 
     def move(self):
         theta_rad = radians(self.theta)
--- sandbox/Ball_trail/ball_with_trail.py
+++ sandbox/Ball_trail/ball_with_trail.py
@@ -6,15 +6,23 @@ screen.fill((0, 0, 0))
 
 running = True
 
-ball = Ball()
+ball = Ball((255, 66, 0))
+ball2 = Ball((153, 48, 12), 270)
+ball3 = Ball((84, 25, 6), 320)
 
 while running:
-    fade = pygame.Surface((640, 640), pygame.SRCALPHA)
-    fade.fill((0, 0, 0, 25))
-    screen.blit(fade, (0, 0))
+    # fade = pygame.Surface((640, 640), pygame.SRCALPHA)
+    # fade.fill((0, 0, 0, 25))
+    # screen.blit(fade, (0, 0))
 
     ball.draw(screen)
     ball.move()
+
+    ball2.draw(screen)
+    ball2.move()
+
+    ball3.draw(screen)
+    ball3.move()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
